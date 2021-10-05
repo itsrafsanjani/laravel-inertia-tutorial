988a9445fed28979f494b0a19811b4a9eb868f97
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-06 00:58:39 +0600

    feat: lesson 3 done

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+
+    <p>In this series, we will use the following frameworks: </p>
+
+    <ul>
+        <li v-for="framework in frameworks">
+            {{ framework }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+};
+</script>
